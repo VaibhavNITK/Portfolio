8790c96b16472a418071ac004b68baf5e67bda44
Vaibhav <vaibhavagarwal693@gmail.com>
2024-01-02 22:51:46 +0530

    final changes

---
+++ src/constants/index.js
@@ -0,0 +1,224 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "Starbucks",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "March 2020 - April 2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "Tesla",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Shopify",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Meta",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "Jan 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/VaibhavNITK',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/vaibhav-agrawal-3037a3223/',
+    },
+    // {
+    //     name: 'LeetCode',
+    //     iconUrl: leetcode,
+    //     link: 'https://leetcode.com/Vaib_Agar/',
+    // }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Codebook',
+        description: 'CodeBook, a dynamic platform, underwent transformative upgrades: tracking upcoming coding contests via integrated APIs, facilitating seamless engagement. Introducing a Mock Interview feature with embedded video calls enriched user experiences, enabling swift interview sessions. Additionally, refined UI enhancements for Codeforces submissions empowered users to effortlessly track and elevate their performance.',
+        link: 'https://github.com/IEEE-Codebook',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Career Climb',
+        description: 'Crafted CareerClimb, a comprehensive web app enabling user, POC, and admin views for streamlined company interactions. Admins wield control, adding companies, reviewing applications, while integrated JWT and cookies ensure robust user privacy and secure authentication.',
+        link: 'https://github.com/VaibhavNITK/CarrerClimb',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Exam Archive',
+        description: 'ExamArchive: Your dynamic educational hub, fostering seamless collaboration and resource sharing. With an intuitive interface, it empowers users to explore, contribute, and engage effortlessly within a vibrant academic community.',
+        link: 'https://github.com/Shubham-Rasal/ExamArchive',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Go Local',
+        description: 'Experience convenience redefined with our local shop app, revolutionizing shopping by offering seamless exploration, item availability checks, and informed decisions, eliminating the need for physical store visits. Effortlessly find nearby shops and browse their items through our user-friendly interface and accurate location tracking.',
+        link: 'https://github.com/IEEE-GoLocal',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'OS Simulator',
+        description: 'Showcased adeptness in memory management, implementing MFT/MVT techniques and diverse allocation algorithms—first fit, best fit, worst fit, and next fit—ensuring optimal system resource utilization. Proficiently managed file systems, employing indexed sequential and contiguous allocation methods to enhance data accessibility and storage efficiency.',
+        link: 'https://github.com/VaibhavNITK/OS_Simulator',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Portfolio',
+        description: 'Crafted a captivating 3D portfolio website featuring engaging sections for about, projects, and an integrated email feature, harmonizing creativity with functionality for a distinctive online presence.',
+        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    }
+];
---
+++ src/pages/About.jsx
@@ -0,0 +1,119 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import { CTA } from "../components";
+import { experiences, skills } from "../constants";
+
+import "react-vertical-timeline-component/style.min.css";
+
+const About = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm{" "}
+        <span className='blue-gradient_text font-semibold drop-shadow'>
+          {" "}
+          Vaibhav
+        </span>{" "}
+        👋
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+        In the bustling tapestry of the digital realm, I'm a student, developer, and contemplative mind hailing from India. My passion resonates within the lines of code that breathe life into websites and elegantly solve real-world problems.
+
+Yet, beyond the luminous screens, I seek solace in the gentle cadence of long walks, where thoughts intertwine like threads weaving through the fabric of my existence. A harmonious blend of technology and philosophy defines my narrative – moments oscillate between immersive coding sessions and capturing fleeting thoughts that dance within the confines of my mind.
+
+My journey isn't just about crafting digital solutions; it's a symphony of curiosity, a perpetual quest to understand the underlying ethos of both code and life itself. Join me in exploring this intersection where technology meets the profound, where each line of code and each pondered thought paint the canvas of my unique journey.
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill) => (
+            <div className='block-container w-20 h-20' key={skill.name}>
+              <div className='btn-back rounded-xl' />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* <div className='py-16'>
+        <h3 className='subhead-text'>Work Experience.</h3>
+        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+          <p>
+            I've worked with all sorts of companies, leveling up my skills and
+            teaming up with smart people. Here's the rundown:
+          </p>
+        </div>
+
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {experiences.map((experience, index) => (
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                iconStyle={{ background: experience.iconBg }}
+                icon={
+                  <div className='flex justify-center items-center w-full h-full'>
+                    <img
+                      src={experience.icon}
+                      alt={experience.company_name}
+                      className='w-[60%] h-[60%] object-contain'
+                    />
+                  </div>
+                }
+                contentStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: "none",
+                }}
+              >
+                <div>
+                  <h3 className='text-black text-xl font-poppins font-semibold'>
+                    {experience.title}
+                  </h3>
+                  <p
+                    className='text-black-500 font-medium text-base'
+                    style={{ margin: 0 }}
+                  >
+                    {experience.company_name}
+                  </p>
+                </div>
+
+                <ul className='my-5 list-disc ml-5 space-y-2'>
+                  {experience.points.map((point, index) => (
+                    <li
+                      key={`experience-point-${index}`}
+                      className='text-black-500/50 font-normal pl-1 text-sm'
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div> */}
+
+      <hr className='border-slate-200' />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default About;
